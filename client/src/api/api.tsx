8bcd521cd21f41b49e381f834831ec339dd6b3df
willianderson <106893704+Willian1661@users.noreply.github.com>
2023-08-21 10:07:19 -0300

    consume api to get users (#4)
    * added files
    
    * changes:
    - created fecthData function
    - changed some string types to number on User interfaces

---
+++ client/src/api/api.tsx
@@ -0,0 +1,16 @@
+import User from "../interfaces/Users"
+export const fetchData = async () => {
+    try {
+
+        const response = await fetch('https://jsonplaceholder.typicode.com/users')
+
+        const data: User[] = await response.json()
+
+        data.map(userdata => console.log(userdata))
+        return data;
+
+    } catch (error) {
+        console.error("Error fetching data", error)
+        throw error
+    }
+}
