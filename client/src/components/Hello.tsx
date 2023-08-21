de8d4cd9ac6969e4374117f2879490eefd38231e
willianderson <106893704+Willian1661@users.noreply.github.com>
2023-08-21 10:46:29 -0300

    - modifications: (#3)
    - fixed input/output files for build
    - renamed pages files
    - changes:
    - created components for main screen
    - modified index.tsx

--- client/src/components/Hello.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-const Hello = () => {
-    return (
-        <p>
-            this is a component
-        </p>
-    )
-}
-export default Hello;
---
+++ client/src/pages/app.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Nav from '../components/Header';
+import Table from '../components/Table';
+import Footer from '../components/Footer';
+const App: React.FC = () => {
+    return (
+        <>
+            <Nav />
+            <Table />
+            <Footer/>
+        </>
+    )
+}
+export default App;
